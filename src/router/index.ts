e715743273800125d97da43f6707bbaf33d1f17e
taru <taruru1010@gmail.com>
2021-02-14 07:27:56 +0900

    Home to Menu

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,11 +1,11 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
-import Home from '../views/Home.vue'
+import Menu from '../views/Menu.vue'
 
 const routes: Array<RouteRecordRaw> = [
   {
     path: '/',
-    name: 'Home',
-    component: Home
+    name: 'Menu',
+    component: Menu
   },
   {
     path: '/about',
